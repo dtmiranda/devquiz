a8575db7471634f65972d6301f7531e31ee42c43
dtmiranda <hortadito2@gmail.com>
2022-01-30 12:44:46 -0100

    CREATE: splash page

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,10 +1,12 @@
+import 'package:devquiz/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: "DevQuiz",
+      home: SplashPage(),
     );
   }
 }
